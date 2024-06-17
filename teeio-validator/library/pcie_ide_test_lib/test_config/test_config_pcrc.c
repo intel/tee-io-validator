d0be1615a3a3a888d7b23f07c1ad6ab51d9ea961
Min M Xu <min.m.xu@intel.com>
2024-06-16 21:18:19 -0400

    Update pcie_ide_lib and pcie_ide_test_lib
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- teeio-validator/library/pcie_ide_test_lib/test_config/test_config_pcrc.c
+++ teeio-validator/library/pcie_ide_test_lib/test_config/test_config_pcrc.c
@@ -39,9 +39,9 @@ static bool test_config_set_pcrc_common(void *test_context, bool enable)
   TEEIO_DEBUG((TEEIO_DEBUG_INFO, "%s pcrc for %s\n", enable ? "enable" : "disable", m_ide_test_topology_name[top->type]));
 
   ide_common_test_port_context_t *port_context = &group_context->upper_port;
-  set_pcrc_in_ecap(port_context->cfg_space_fd, ide_type, port_context->ide_id, port_context->ecap_offset, enable);
+  set_pcrc_in_ecap(port_context->cfg_space_fd, ide_type, port_context->priv_data.pcie.ide_id, port_context->ecap_offset, enable);
   port_context = &group_context->lower_port;
-  set_pcrc_in_ecap(port_context->cfg_space_fd, ide_type, port_context->ide_id, port_context->ecap_offset, enable);
+  set_pcrc_in_ecap(port_context->cfg_space_fd, ide_type, port_context->priv_data.pcie.ide_id, port_context->ecap_offset, enable);
 
   return true;
 }
@@ -54,8 +54,8 @@ static bool test_config_check_pcrc_support_common(void *test_context)
   ide_common_test_group_context_t *group_context = config_context->group_context;
   TEEIO_ASSERT(group_context->signature == GROUP_CONTEXT_SIGNATURE);
 
-  PCIE_IDE_CAP *host_cap = &group_context->upper_port.ide_cap;
-  PCIE_IDE_CAP *dev_cap = &group_context->lower_port.ide_cap;
+  PCIE_IDE_CAP *host_cap = &group_context->upper_port.priv_data.pcie.ide_cap;
+  PCIE_IDE_CAP *dev_cap = &group_context->lower_port.priv_data.pcie.ide_cap;
   if(!host_cap->pcrc_supported || !dev_cap->pcrc_supported) {
     TEEIO_DEBUG((TEEIO_DEBUG_WARN, "check pcrc and it is NOT supported. host=%d, device=%d\n", host_cap->pcrc_supported, dev_cap->pcrc_supported));
     return false;
