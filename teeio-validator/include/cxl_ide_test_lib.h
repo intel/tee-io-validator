c17a63154e9a68c953f1e2edd93d96114e989f1c
Min M Xu <min.m.xu@intel.com>
2024-07-03 20:11:16 -0400

    Add cxl related header files
    Signed-off-by: Min Xu <min.m.xu@intel.com>

---
+++ teeio-validator/include/cxl_ide_test_lib.h
@@ -0,0 +1,28 @@
+/**
+ *  Copyright Notice:
+ *  Copyright 2024 Intel. All rights reserved.
+ *  License: BSD 3-Clause License.
+ **/
+
+#ifndef __CXL_IDE_TEST_LIB_H__
+#define __CXL_IDE_TEST_LIB_H__
+
+#include "ide_test.h"
+
+// cxl_ide_test_lib header file
+
+bool cxl_ide_test_lib_register_funcs(
+  teeio_test_case_funcs_t* case_funcs,
+  teeio_test_config_funcs_t* config_funcs,
+  teeio_test_group_funcs_t* group_funcs);
+
+ide_test_case_name_t*
+cxl_ide_test_lib_get_test_case_names(
+  int* cnt);
+
+uint32_t
+cxl_ide_test_lib_get_config_bitmask(
+  int* config_type_num,
+  IDE_TEST_TOPOLOGY_TYPE top_type);
+
+#endif
