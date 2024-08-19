880ddaf8efe241bcc1bb74e85b67e24cd5ff33ed
Min M Xu <min.m.xu@intel.com>
2024-08-19 04:13:20 -0400

    Add cxl ide test case/config funcs
    These funcs are placeholders and will be implemented in the future.
    
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- teeio-validator/library/cxl_ide_test_lib/include/cxl_ide_test_common.h
+++ teeio-validator/library/cxl_ide_test_lib/include/cxl_ide_test_common.h
@@ -15,6 +15,68 @@ bool cxl_ide_test_full_ide_stream_setup(void *test_context);
 bool cxl_ide_test_full_ide_stream_run(void *test_context);
 bool cxl_ide_test_full_ide_stream_teardown(void *test_context);
 
+// Query
+bool cxl_ide_test_query_1_setup(void *test_context);
+bool cxl_ide_test_query_1_run(void *test_context);
+bool cxl_ide_test_query_1_teardown(void *test_context);
+
+bool cxl_ide_test_query_2_setup(void *test_context);
+bool cxl_ide_test_query_2_run(void *test_context);
+bool cxl_ide_test_query_2_teardown(void *test_context);
+
+// KEY_PROG
+bool cxl_ide_test_key_prog_1_setup(void *test_context);
+bool cxl_ide_test_key_prog_1_run(void *test_context);
+bool cxl_ide_test_key_prog_1_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_2_setup(void *test_context);
+bool cxl_ide_test_key_prog_2_run(void *test_context);
+bool cxl_ide_test_key_prog_2_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_3_setup(void *test_context);
+bool cxl_ide_test_key_prog_3_run(void *test_context);
+bool cxl_ide_test_key_prog_3_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_4_setup(void *test_context);
+bool cxl_ide_test_key_prog_4_run(void *test_context);
+bool cxl_ide_test_key_prog_4_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_5_setup(void *test_context);
+bool cxl_ide_test_key_prog_5_run(void *test_context);
+bool cxl_ide_test_key_prog_5_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_6_setup(void *test_context);
+bool cxl_ide_test_key_prog_6_run(void *test_context);
+bool cxl_ide_test_key_prog_6_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_7_setup(void *test_context);
+bool cxl_ide_test_key_prog_7_run(void *test_context);
+bool cxl_ide_test_key_prog_7_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_8_setup(void *test_context);
+bool cxl_ide_test_key_prog_8_run(void *test_context);
+bool cxl_ide_test_key_prog_8_teardown(void *test_context);
+
+bool cxl_ide_test_key_prog_9_setup(void *test_context);
+bool cxl_ide_test_key_prog_9_run(void *test_context);
+bool cxl_ide_test_key_prog_9_teardown(void *test_context);
+
+// KSET_GO
+bool cxl_ide_test_kset_go_1_setup(void *test_context);
+bool cxl_ide_test_kset_go_1_run(void *test_context);
+bool cxl_ide_test_kset_go_1_teardown(void *test_context);
+
+// KSET_STOP
+bool cxl_ide_test_kset_stop_1_setup(void *test_context);
+bool cxl_ide_test_kset_stop_1_run(void *test_context);
+bool cxl_ide_test_kset_stop_1_teardown(void *test_context);
+
+// GET_KEY
+// KSET_GO
+bool cxl_ide_test_get_key_1_setup(void *test_context);
+bool cxl_ide_test_get_key_1_run(void *test_context);
+bool cxl_ide_test_get_key_1_teardown(void *test_context);
+
 //
 // CXL_IDE Test Config
 //
@@ -25,13 +87,25 @@ bool cxl_ide_test_config_default_disable(void *test_context);
 bool cxl_ide_test_config_default_support(void *test_context);
 bool cxl_ide_test_config_default_check(void *test_context);
 
+// pcrc config
+bool cxl_ide_test_config_pcrc_enable(void *test_context);
+bool cxl_ide_test_config_pcrc_disable(void *test_context);
+bool cxl_ide_test_config_pcrc_support(void *test_context);
+bool cxl_ide_test_config_pcrc_check(void *test_context);
+
+// ide_stop config
+bool cxl_ide_test_config_ide_stop_enable(void *test_context);
+bool cxl_ide_test_config_ide_stop_disable(void *test_context);
+bool cxl_ide_test_config_ide_stop_support(void *test_context);
+bool cxl_ide_test_config_ide_stop_check(void *test_context);
+
 // skid mode
 bool cxl_ide_test_config_skid_enable(void *test_context);
 bool cxl_ide_test_config_skid_disable(void *test_context);
 bool cxl_ide_test_config_skid_support(void *test_context);
 bool cxl_ide_test_config_skid_check(void *test_context);
 
-// skid mode
+// containment mode
 bool cxl_ide_test_config_containment_enable(void *test_context);
 bool cxl_ide_test_config_containment_disable(void *test_context);
 bool cxl_ide_test_config_containment_support(void *test_context);
---
+++ teeio-validator/library/cxl_ide_test_lib/test_config/test_config_ide_stop.c
@@ -0,0 +1,42 @@
+/**
+ *  Copyright Notice:
+ *  Copyright 2023-2024 Intel. All rights reserved.
+ *  License: BSD 3-Clause License.
+ **/
+
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "hal/base.h"
+#include "hal/library/debuglib.h"
+
+#include "ide_test.h"
+#include "helperlib.h"
+#include "teeio_debug.h"
+#include "cxl_ide_lib.h"
+#include "cxl_ide_test_common.h"
+
+// test config of IDE Stop
+bool cxl_ide_test_config_ide_stop_enable(void *test_context)
+{
+  NOT_IMPLEMENTED(__func__);
+  return false;
+}
+
+bool cxl_ide_test_config_ide_stop_disable(void *test_context)
+{
+  NOT_IMPLEMENTED(__func__);
+  return false;
+}
+
+bool cxl_ide_test_config_ide_stop_support(void *test_context)
+{
+  NOT_IMPLEMENTED(__func__);
+  return false;
+}
+
+bool cxl_ide_test_config_ide_stop_check(void *test_context)
+{
+  NOT_IMPLEMENTED(__func__);
+  return false;
+}
