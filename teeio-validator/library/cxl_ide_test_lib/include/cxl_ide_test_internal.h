02246df39e02055e6ef4471cbe2cdaef64b0d22a
Min M Xu <min.m.xu@intel.com>
2024-12-09 18:45:13 +0800

    Implement CXL Key Refresh
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- teeio-validator/library/cxl_ide_test_lib/include/cxl_ide_test_internal.h
+++ teeio-validator/library/cxl_ide_test_lib/include/cxl_ide_test_internal.h
@@ -33,7 +33,8 @@ bool cxl_setup_ide_stream(void *doe_context, void *spdm_context,
                           uint8_t stream_id, uint8_t port_index,
                           ide_common_test_port_context_t *upper_port,
                           ide_common_test_port_context_t *lower_port,
-                          bool skip_ksetgo, uint32_t config_bitmap);
+                          bool skip_ksetgo, uint32_t config_bitmap,
+                          bool set_link_enc_enable, bool program_iv);
 // stop cxl ide stream
 bool cxl_stop_ide_stream(void *doe_context, void *spdm_context,
                          uint32_t *session_id, uint8_t *kcbar_addr,
