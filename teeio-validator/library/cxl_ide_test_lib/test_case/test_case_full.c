f1bfa118953b3068fe345ca027468629f63daf65
Min M Xu <min.m.xu@intel.com>
2024-12-09 18:45:13 +0800

    Implement CXL Key Refresh
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- teeio-validator/library/cxl_ide_test_lib/test_case/test_case_full.c
+++ teeio-validator/library/cxl_ide_test_lib/test_case/test_case_full.c
@@ -27,7 +27,8 @@ bool cxl_setup_ide_stream(void *doe_context, void *spdm_context,
                           uint8_t stream_id, uint8_t port_index,
                           ide_common_test_port_context_t *upper_port,
                           ide_common_test_port_context_t *lower_port,
-                          bool skip_ksetgo, uint32_t config_bitmap);
+                          bool skip_ksetgo, uint32_t config_bitmap,
+                          bool set_link_enc_enable, bool program_iv);
 bool cxl_stop_ide_stream(void *doe_context, void *spdm_context,
                          uint32_t *session_id, uint8_t *kcbar_addr,
                          uint8_t stream_id,
@@ -57,7 +58,7 @@ bool cxl_ide_test_full_ide_stream_setup(void *test_context)
   return cxl_setup_ide_stream(group_context->spdm_doe.doe_context, group_context->spdm_doe.spdm_context,
                               &group_context->spdm_doe.session_id, upper_port->mapped_kcbar_addr,
                               group_context->stream_id, 0,
-                              upper_port, lower_port, false, configuration->bit_map);
+                              upper_port, lower_port, false, configuration->bit_map, true, true);
 }
 
 bool cxl_ide_test_full_ide_stream_run(void *test_context)
