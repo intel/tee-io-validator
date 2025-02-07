359178622a4fc2af7ac554597cbd8b7b9ba40de0
Min M Xu <min.m.xu@intel.com>
2025-02-07 19:12:57 +0800

    Delete duplicated print information
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- teeio-validator/library/cxl_ide_test_lib/test_case/ide_km_common.c
+++ teeio-validator/library/cxl_ide_test_lib/test_case/ide_km_common.c
@@ -150,17 +150,6 @@ bool cxl_stop_ide_stream(void *doe_context, void *spdm_context,
   return true;
 }
 
-static void dump_cxl_ide_status(ide_common_test_port_context_t* upper_port, ide_common_test_port_context_t* lower_port)
-{
-  TEEIO_PRINT(("\n"));
-  TEEIO_PRINT(("Print host cxl ide status\n"));
-  cxl_dump_ide_status(upper_port->cxl_data.memcache.cap_headers, upper_port->cxl_data.memcache.cap_headers_cnt, upper_port->cxl_data.memcache.mapped_memcache_reg_block);
-
-  TEEIO_PRINT(("\n"));
-  TEEIO_PRINT(("Print device cxl ide status.\n"));
-  cxl_dump_ide_status(lower_port->cxl_data.memcache.cap_headers, lower_port->cxl_data.memcache.cap_headers_cnt, lower_port->cxl_data.memcache.mapped_memcache_reg_block);
-}
-
 // setup cxl ide stream
 bool cxl_setup_ide_stream(void *doe_context, void *spdm_context,
                           uint32_t *session_id, uint8_t *kcbar_addr,
@@ -326,9 +315,5 @@ bool cxl_setup_ide_stream(void *doe_context, void *spdm_context,
   // wait for 10 ms for device to get ide ready
   libspdm_sleep(10 * 1000);
 
-  dump_cxl_ide_status(upper_port, lower_port);
-
-  printf("cxl_setup_ide_stream is done. Press any key to continue.\n");
-  getchar();
   return true;
 }
